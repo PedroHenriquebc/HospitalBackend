6778cd576b11d79d01eb3b6357dcdb4a4c0d9d28
matheus-gui <matguimara0706@gmail.com>
2020-09-14 22:18:08 -0300

    Change in table prescreve.

--- src/main/java/com/project/hospital/model/Prescreve.java
+++ src/main/java/com/project/hospital/model/Prescreve.java
@@ -19,7 +19,35 @@ public class Prescreve {
 	
 	@Column(name = "Suspender")
 	private Integer supender;
+
+	public Prescreve() {
+		super();
+	}
+
+	public Integer getDosagem() {
+		return dosagem;
+	}
+
+	public void setDosagem(Integer dosagem) {
+		this.dosagem = dosagem;
+	}
+
+	public Time getHorario() {
+		return horario;
+	}
+
+	public void setHorario(Time horario) {
+		this.horario = horario;
+	}
+
+	public Integer getSupender() {
+		return supender;
+	}
+
+	public void setSupender(Integer supender) {
+		this.supender = supender;
+	}
 	
-		
+	
 		
 }
