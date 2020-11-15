a7ed106f2e78583e991d009a57ec04fd316b9ee7
Pedro Henrique <pedrohbc96@gmail.com>
2020-11-15 14:45:14 -0300

    Apaga linha de comandos não utilizadas

--- src/main/java/com/project/hospital/model/Leito.java
+++ src/main/java/com/project/hospital/model/Leito.java
@@ -1,8 +1,6 @@
 package com.project.hospital.model;
 
-import java.util.List;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
