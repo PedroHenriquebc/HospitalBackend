a27a0201bb54155810439bb46b56c1422fd4db2c
Pedro Henrique <pedrohbc96@gmail.com>
2020-11-15 14:45:52 -0300

    Adiciona ''extends JpaRepository" que estavam faltando e causando erro

--- src/main/java/com/project/hospital/repository/PrescreveRepository.java
+++ src/main/java/com/project/hospital/repository/PrescreveRepository.java
@@ -1,8 +1,11 @@
 package com.project.hospital.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.project.hospital.model.Prescreve;
+
 @Repository
-public interface PrescreveRepository {
+public interface PrescreveRepository extends JpaRepository<Prescreve, Long> {
 	
 }
